2e5f3db825021b1dd12578388881477adb3af6c2
Oscar Gordillo <oscar_andres16@hotmail.com>
2019-11-12 20:30:10 -0600

    Functions and functions factory

--- index.js
+++ index.js
@@ -1,111 +1,34 @@
-/*
-const myName = "Oscar";
-Template Literal
-console.log(`Hello World! this is ${myName}`);
+//FUNCTIONS
+function doMath(x, y, mathFxn) {
+  console.trace(); //Call stack
+  return mathFxn(x, y);
+}
+function add(x, y) {
+  console.trace();
+  return x + y;
+}
+
+function substract(x, y) {
+  console.trace();
+  return x - y;
+}
+
+console.log(doMath(10, 5, add));
+console.log(doMath(10, 5, substract));
+
+/**
+function calling another function.
+This is a function factory that returns another function
+This FUNCTION FACTORY uses the function of CLOSURE
 */
-//Object literal
-const name = 'Oscar';
-
-const me = {
-  name: "Oscar",
-  age: 24,
-  athlete: true,
-  weight: 170,
-  checkDrinkingAge: function() {
-    return this.age >= 21;
-  }
-
-};
-console.log(me.checkDrinkingAge())
-/*Bracket Notation
-console.log(`I am ${name}`);
-
-//Dot notation
-console.log(me.age);
-
-
-const object1 = {
-  id_num : 1234,
-  "myFunc" : function (fullName){
-    console.log(`Thanks for your purchase ${fullName}`);
-  },
-};
-
-object1.myFunc("Oscar Gordillo");
-
-for (i = 0; i <= 10; i++) {
-  counter = ++;
-  console.log(i);
+function initSportsScorer(pts) {
+  return function(score) {
+    return score + pts;
+  };
 }
-Add the numbers from 1 to 10
-*/
-// while loop
-let currentNum;
-let currentTotal = 0;
 
-while(currentNum <= 10){
-  currentTotal += currentNum;
-  currentNum += 1;
-}
-console.log(currentTotal)
-// for loop
-let currentTotal = 0;
+const threePointer = initSportsScorer(3);
+const touchdown = initSportsScorer(7);
 
-for (i = 1; i <= 10; i += 1) {
-  console.log('i is', i);
-  currentTotal += i;
-  console.log('currentTotal is', currentTotal);
-}
-console.log(currentTotal);
-
-const numberSummerMachine = {
-  startingNumber: 1,
-  endingNumber: 10,
-  sumDaNumbers: function() {
-    let total = 0;
-
-    for (let i = this.startingNumber; i <= this.endingNumber; i += 1) {
-    total += i;
-    }
-
-    return total;
-  }
-};
-
-
-//setter that allows updating of the 'local data' RESEARCH
-setStatingNumber (num) {
-  this.startingNumber = num;
- }
-
-console.log(numberSummerMachine.sumDaNumbers())
-
-
-// Another solution using while loops
-
-
-const numberSummerMachine = {
-  startingNumber: 1,
-  endingNumber: 10,
-  sumDaNumbers: function() {
-    let total = 0;
-    let i = this.startingNumber;
-
-    while(i <= this.endingNumber) {
-    total += i;
-    i += 1;
-    }
-
-    return total;
-  },
-
-  setStatingNumber (num) {
-    this.startingNumber = num;
-   }
-
-  console.log(numberSummerMachine.sumDaNumbers())
-};
-
-
-
-
+console.log(threePointer(99));
+console.log(touchdown(7));
