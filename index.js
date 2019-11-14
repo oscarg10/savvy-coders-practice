7e82578a5fae853f67018943fa8c66b36926b3a0
Oscar Gordillo <oscar_andres16@hotmail.com>
2019-11-13 21:11:51 -0600

    HW4 solution

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-//FUNCTIONS
-function doMath(x, y, mathFxn) {
-  console.trace(); //Call stack
-  return mathFxn(x, y);
-}
-function add(x, y) {
-  console.trace();
-  return x + y;
-}
-
-function substract(x, y) {
-  console.trace();
-  return x - y;
-}
-
-console.log(doMath(10, 5, add));
-console.log(doMath(10, 5, substract));
-
-/**
-function calling another function.
-This is a function factory that returns another function
-This FUNCTION FACTORY uses the function of CLOSURE
-*/
-function initSportsScorer(pts) {
-  return function(score) {
-    return score + pts;
+//HW4 solution
+function initGreeting(greeting, name) {
+  return function() {
+    return greeting + name;
   };
 }
-
-const threePointer = initSportsScorer(3);
-const touchdown = initSportsScorer(7);
-
-console.log(threePointer(99));
-console.log(touchdown(7));
+const morningMartin = initGreeting("Good Morning, ", "Martin");
+//morningMartin became a function when initGreeting was assigned
+console.log(morningMartin());
