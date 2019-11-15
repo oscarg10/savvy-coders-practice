ccc282e618409dd8a2ad9c544c45c0b1f8f6c0ff
Oscar Gordillo <oscar_andres16@hotmail.com>
2019-11-14 20:38:41 -0600

    Add Recursive solutions

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-//Recursion
-function writePsychoBook(n) {
-  console.log("invoking psycho books is now", n);
+function myPow(num, exp) {
+  let ret = num;
 
-  if (n === 0) {
-    return "I am done";
+  for (let i = 1; i < exp; i += 1) {
+    ret *= num;
   }
-
-  return `All work and no play makes Jack a dull boy ${writePsychoBook(n - 1)}`;
+  return ret;
 }
 
-console.log(writePsychoBook(99));
+console.log(myPow(2, 3));
+
+//recursion
+
+function myPow(num, exp) {
+
+
+  if (exp === 0) { //starts at cero because it is the kick out.
+    return 1;
+  }
+  return (num *= myPow(num, exp - 1));
+}
+
+console.log(myPow(2,3));
+
+/* factorial exercise
+for loop can count backwards
+*/
+function factorial(num) {
+  let ret = 1;
+
+  for (let i = num; i >= 1; i -= 1) {
+    ret *= i;
+  }
+
+  return ret;
+}
+
+console.log(factorial(4));
+
+//Recursive solution
+function factorial(num) {
+
+
+  if (num === 0) { //starts at cero because it is the kick out.
+    return 1;
+  }
+  return num * factorial(num,  - 1);
+}
+
+console.log(factorial(4));
