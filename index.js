4af4648d92cd785c87a7fd8d01b32851d6a6fe27
Oscar Gordillo <oscar_andres16@hotmail.com>
2019-11-14 19:23:26 -0600

    Add Psycho Books solution

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function initGreeting(name, greeting) {
-  return function(time) {
-    return `${name} ${greeting}. The time is: ${time}`;
-  };
+const allWork = "All work and no play makes Jack a dull";
+
+function writePsychoBook(num) {
+  let str = "";
+  for (i = 0; i <= num; i += 1) {
+    str += allWork;
+  }
+  return str;
 }
 
-const eveningGeorge = initGreeting("George", "Evening");
-
-const time = new Date(Date.now());
-//getHours and getMinutes are methods that belong to 'Date'
-console.log(eveningGeorge(`${time.getHours()}:${time.getMinutes()}`));
+console.log(writePsychoBook(10));
