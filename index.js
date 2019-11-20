4298cac6475a860d89483e56fecffa4ac98f7525
Oscar Gordillo <oscar_andres16@hotmail.com>
2019-11-19 20:36:10 -0600

    Add prototype examples

--- index.js
+++ index.js
@@ -1,55 +1,33 @@
-function shoppingList(obj1, obj2, obj3) {
-  return {
-    food: obj1,
-    clothes: obj2,
-    cars: obj3,
-
-    getParam1: () => `Hello ${shoppingList.food}`,
-    getParam2: function() {
-      return `hello again ${this.clothes}`;
-    },
-    //this won't work because it looks for the global 'cars' which is undefined
-    getCars: () => `Bye ${this.cars} `
-  };
-}
-
-const myObj = shoppingList("pizza", "t-shirt", "BMW");
-
-console.log(myObj);
-console.log(myObj.getParam1());
-console.log(myObj.getParam2());
-console.log(myObj.getCars());
-
-//Function Constructor
-
 function Food(breakfast, lunch, dinner) {
   this.breakfast = breakfast;
   this.lunch = lunch;
   this.dinner = dinner;
+}
+Food.prototype.breakfastDrink = function() {
+  if (this.breakfast === "milk") {
+    return `I would like some ${this.breakfast} with my breakfast`;
+  }
+  return `I'm lactose intolerant, can I have some ${this.breakfast} instead`;
+};
+Food.prototype.eatOut = function() {
+  if (this.lunch === "yes") {
+    return "I'd love to get lunch at my favorite restaurant";
+  }
+  return "I wanna cook my own lunch";
+};
 
-  this.breakfastDrink = function() {
-    if (this.breakfast === "milk") {
-      return `I would like some ${breakfast} with my breakfast`;
-    }
-    return `I'm lactose intolerant, can I have some ${breakfast} instead`;
-  };
-  this.eatOut = function() {
-    if (this.lunch === "yes") {
-      return "I'd love to get lunch at my favorite restaurant";
-    }
-    return "I wanna cook my own lunch";
-  };
-  //This won't work because of the arrow function
-  this.eatDinner () => `I will cook some ${dinner} for dinner`;
-  };
-
+Food.prototype.eatDinner = function() {
+  return `I will cook some ${this.dinner} for dinner`;
+};
+Food.prototype.setSnack = function(snack) {
+  if (snack && typeof snack === "string") {
+    this.snack = snack;
+  } else {
+    console.error("Snack cannot be blank");
+  }
+};
 
 const foodPlan = new Food("milk", "yes", "pasta");
-console.log(foodPlan);
-console.log(foodPlan.breakfastDrink());
-console.log(foodPlan.eatOut());
-console.log(foodPlan.eatDinner());
-
-
-// More examples
-
+console.log("before set snack", foodPlan);
+foodPlan.setSnack("chips");
+console.log("after set snack", foodPlan);
