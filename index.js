31cf2676714a67eecc233f66a43dcd957c06acf8
Oscar Gordillo <oscar_andres16@hotmail.com>
2019-11-26 18:37:39 -0600

    Add more class examples

--- index.js
+++ index.js
@@ -1,120 +1,33 @@
-// class Keyword
+// Class
 
-class Food {
-  //does not have any parenthesis
-  constructor(breakfast, lunch, dinner) {
-    this.breakfast = breakfast;
-    this.lunch = lunch;
-    this.dinner = dinner;
+class Person {
+  constructor(firstName, lastName, age, position) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.position = position;
   }
-
-  get breakfastDrinks() {
-    return `I would like some ${this.breakfast} with my breakfast`;
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
-
-  get eatout() {
-    if (this.lunch === "yes") {
-      return "I'd love to get lunch at my favorite restaurant";
-    }
-    return "I wanna cook my own lunch";
-  }
-
-  get eatDinner() {
-    return `I will cook some ${this.dinner} for dinner`;
-  }
-
-  set changeBreakfast(newBreakfastDrinks) {
-    if (newBreakfastDrinks) {
-      this.breakfast = newBreakfastDrinks;
+  set changeName(newLname) {
+    if (newLname) {
+      this.lastName = newLname;
     } else {
-      console.error("We don't have it available");
+      console.error("this is not working");
     }
   }
 }
-const foodPlan = new Food("milk", "yes", "pasta");
-// console.log(foodPlan.eatout);
 
-console.log((foodPlan.changeBreakfast = ""));
-
-class foodPlan extends Person {
-  constructor ()
-}
-
-console.log((foodPlan.changeBreakfast = ""));
-console.log(foodPlan.changeBreakfast);
-
-console.log(foodPlan.calories);
-
-function Random(breakfast, lunch, dinner, idk) {
-  Food.call(breakfast, lunch, dinner);
-  this.uhum = idk;
-}
-
-const dome = new Random("milk", "yes", "pasta", "rest");
-
-console.log(dome);
-// function constructor does not do anything
-// New keyword is used to invoke FC
-
-function Food(breakfast, lunch, dinner) {
-  this.breakfast = breakfast;
-  this.lunch = lunch;
-  this.dinner = dinner;
-}
-
-Food.prototype.setCalories (numberCalories){
-  this.calories = numberCalories;
-}
-  const betterCalories = new Food ('drinks', 'restaurants', 'meals');
-
-  let breakfastDrinks = ['orange juice', 'milk', 'coffee', 'water'];
-
-  betterCalories.setCalories (breakfastDrinks);
-  Food.prototype.printDrinks() {
-    for (let i = 0; i >= this.calories.length; i += 1){
-      console.log(this.numbers[i]);
-    }
-
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, empid) {
+    super(firstName, lastName, age, position);
+    this.id = empid;
   }
-
-//   breakfastDrinks.printDrinks();
-
-function Meal(drink, entree, dessert) {
-  this.drink = drink;
-  this.entree = entree;
-  this.dessert = dessert;
 }
-
-Meal.prototype.setCalories = function(numbCalories) {
-  this.calories = numbCalories;
-};
-const foodPlan = new Meal("coffee", "steak", "cake");
-
-const totalCals = ["80", "450", "800"];
-
-foodPlan.setCalories(totalCals);
-
-Meal.prototype.printCals = function() {
-  for (let i = 0; i < this.calories.lenght; i += 1) {
-    console.log(this.calories[i]);
-  }
-};
-
-function Dinner(drink, entree, dessert, aptzr) {
-  Meal.call(this, drink, entree, dessert);
-  this.appetizer = aptzr;
-}
-
-const din = new Dinner("soda", "pasta", "cookies", "cheese sticks");
-
-console.log(din);
-
-foodPlan.printCals();
-console.log(foodPlan.printCals(100));
-console.log(foodPlan);
-
-/* Questions
-1. Why do we use a different array name in the setter "numbCalories", than the variable's name that stores the actual array?
-2. Why do we use 'Meal' instead of 'foodPlan' in the printing function "Meal.prototype.printCals"?
-3. How do I assign those calories in the array to each specific item, in this case "coffee", "steak", and "cake"?
-*/
+const emp = new Employee("Andrew", "White", 25, "Teacher", 123);
+const persona = new Person("Andrew", "White", 25, "Teacher");
+console.log(persona.fullName);
+persona.changeName = "Miguel";
+console.log(persona.fullName);
+console.log(emp);
