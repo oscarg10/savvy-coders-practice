4b8c6674bd442d6d5cf34c725bb40dce716623c5
Oscar Gordillo <oscar_andres16@hotmail.com>
2019-11-26 20:01:11 -0600

    refactor using arrow function

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const strings = ["hello", "world", "again"];
 //concat does not mutate elements, but you can still join or combine arrays
 const numbersAndStrings = numbers.concat(strings);
 
+//Function expression.
 function newArray(originalArr, stringUpdate, numberUpdate) {
   const newArray = [];
 
-  originalArr.forEach(function(el) {
+  originalArr.forEach(el => {
     if (typeof el === "string") {
       newArray.push((el += stringUpdate));
     } else {
@@ -21,3 +22,8 @@ function newArray(originalArr, stringUpdate, numberUpdate) {
 }
 
 console.log(newArray(numbersAndStrings, " Hello", 236));
+
+//arrow syntax does not have any this reference.
+numbers.forEach(number => {
+  console.log(number);
+});
