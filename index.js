8da0b28070002acd665a854a78a9d3eeebb24314
Oscar Gordillo <oscar_andres16@hotmail.com>
2019-11-26 19:03:56 -0600

    Add Array concatenation exercise

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-// Class
+// // Arrays
 
-class Person {
-  constructor(firstName, lastName, age, position) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.position = position;
-  }
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  set changeName(newLname) {
-    if (newLname) {
-      this.lastName = newLname;
-    } else {
-      console.error("this is not working");
-    }
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const strings = ["hello", "world", "again"];
+// const newArray = [];
+
+// //concat does not mutate elements, but you can still join or combine arrays
+// const numbersAndStrings = numbers.concat(strings);
+
+// //functional programming: Do not mutate any data ever (ideal)
+
+// for (let i = 0; i < numbersAndStrings.length; i += 1) {
+//   const value = numbersAndStrings.pop();
+//   if (typeof value === "number") {
+//     newArray.push(value + 236);
+//   }
+//   if (typeof value === "string") {
+//     newArray.push(value + "Hello, I am a string");
+//   }
+// }
+
+// console.log(newArray);
+
+// Instructor's solution
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const strings = ["hello", "world", "again"];
+const newArray = [];
+
+//concat does not mutate elements, but you can still join or combine arrays
+const numbersAndStrings = numbers.concat(strings);
+
+for (let i = 0; i < numbersAndStrings.length; i += 1) {
+  if (typeof numbersAndStrings[i] === "string") {
+    newArray.push((numbersAndStrings[i] += "Hello, I am a string"));
+  } else {
+    newArray.push((numbersAndStrings[i] += 236));
   }
 }
 
-class Employee extends Person {
-  constructor(firstName, lastName, age, position, empid) {
-    super(firstName, lastName, age, position);
-    this.id = empid;
-  }
-}
-const emp = new Employee("Andrew", "White", 25, "Teacher", 123);
-const persona = new Person("Andrew", "White", 25, "Teacher");
-console.log(persona.fullName);
-persona.changeName = "Miguel";
-console.log(persona.fullName);
-console.log(emp);
+console.log(newArray);
