738bac0dfdba6029213cfc28f85fd52a6ab3bee3
Oscar Gordillo <oscar_andres16@hotmail.com>
2019-12-02 21:00:46 -0600

    Add HW7 example and map example

--- index.js
+++ index.js
@@ -1,53 +1,36 @@
-const fruits = [
-  "apple",
-  "kiwi",
-  "pineapple",
-  "jackfruit", // index is 3
-  "dragonfruit",
-  "pineapple",
-  "mango",
-  "guava", // index is 7
-  "strawberry"
-];
 
-fruits.splice(3, 3, "raspberries");
-const jackfruitIndex = fruits.indexOf("jackfruit");
-const guavaIndex = fruits.indexOf("guava");
+*/
+//Meal Plan HW 7
+/*
+class Meal {
+  constructor(drink, entree, dessert) {
+    this.drink = drink;
+    this.entree = entree;
+    this.dessert = dessert;
+  }
+  set maxCalories(numCalories) {
+    this.calories = numCalories;
+  }
+  get totalCalories() {
+    for (let i = 0; i < this.calories.lenght; i += 1) {
+      console.log(this.calories[i]);
+    }
+  }
+}
+const foodPlan = new Meal("coffee", "steak", "cake");
+const totalCals = [80, 210, 250];
+foodPlan.maxCalories = totalCals;
+console.log(foodPlan);
+const newCals = totalCals.shift();
+console.log("Coffee has " + newCals + " calories");
+*/
+// Map example
 
-console.log(fruits);
-// console.log(jackfruitIndex, guavaIndex);
+const names = ["Manav", "Stevens", "Jamison", "Wright", "Smith"];
 
-// slice(start, end)
-// splits an array into smaller pieces
-const smallerArray = fruits.slice(jackfruitIndex);
-console.log("smallerArray is", smallerArray);
+// const namesWithMr = names.map(name => `Mr. ${name}`);
 
-const bestFruits = fruits.slice(jackfruitIndex, guavaIndex + 1);
-console.log("bestFruits are", bestFruits);
-
-// includes returns true or false depending on
-// whether or not the argument is in the array
-const mysteryValue = fruits.includes("dragonfruit");
-console.log(mysteryValue);
-
-// indexOf returns the index of the first occurence
-// of our argument in the array
-console.log(fruits.indexOf("pineapple"));
-
-// indexOf returns the index of the last occurence
-// of our argument in the array
-console.log(fruits.lastIndexOf("pineapple"));
-
-const months = ["Jan", "March", "April", "June"];
-
-// Insert 'Feb' WITHOUT removing anything
-months.splice(1, 0, "Feb");
-// inserts at index 1
-console.log(months);
-// expected output: Array ["Jan", "Feb", "March", "April", "June"]
-
-// Insert 'May' after removing the 1 thing at index 4
-months.splice(4, 1, "May");
-// replaces 1 element at index 4
-console.log(months);
-// expected output: Array ["Jan", "Feb", "March", "April", "May"]
+function addSalutation(names, sal) {
+  return names.map(name => `${sal} ${name}`);
+}
+console.log(addSalutation(names, "Mrs."));
